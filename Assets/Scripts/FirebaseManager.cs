462e4e8389d8fa605fbc6d0e14437b8d831463c0
vernonang <39268943+amamitsubasa@users.noreply.github.com>
2024-09-10 17:18:52 +0800

    update2

---
+++ Assets/Scripts/FirebaseManager.cs
@@ -0,0 +1,239 @@
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Firestore;
+using TMPro;
+using UnityEngine.Rendering.Universal;
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using System.ComponentModel;
+using TMPro.EditorUtilities;
+using Firebase.Extensions;
+using NUnit.Framework;
+
+[FirestoreData]
+public struct UserData
+{
+    [FirestoreProperty]
+    public string UserName { get; set; }
+
+    [FirestoreProperty]
+    public string Email { get; set; }
+}
+
+public class FirebaseManager : MonoBehaviour
+{
+    public static FirebaseManager instance;
+
+    // Firebase Variables
+    [Header("Firebase")]
+    public DependencyStatus dependencyStatus;
+    public FirebaseAuth auth;
+    public FirebaseUser User;
+    public string collection = "userData";
+
+    // Login Variables
+    [Header("Login")]
+    public TMP_InputField emailLoginField;
+    public TMP_InputField passwordLoginField;
+    public TMP_Text warningLoginText;
+    public TMP_Text confirmLoginText;
+
+    // Register Variables
+    [Header("Register")]
+    public TMP_InputField usernameRegisterField;
+    public TMP_InputField emailRegisterField;
+    public TMP_InputField passwordRegisterField;
+    public TMP_InputField passwordRegisterVerifyField;
+    public TMP_Text warningRegisterText;
+
+    // Logged In Variables
+    [Header("Logged In")]
+    public TMP_Text userInfoText;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            dependencyStatus = task.Result;
+            if(dependencyStatus == DependencyStatus.Available)
+            {
+                InitializeFirebase();
+            }
+            else
+            {
+                Debug.LogError("Could not resolve all Firebase dependencies: " + dependencyStatus);
+            }
+        });
+    }
+
+    public void InitializeFirebase()
+    {
+        Debug.Log("Setting up Firebase Auth");
+        auth = FirebaseAuth.DefaultInstance;
+        
+    }
+
+    public void SetData()
+    {
+        var userData = new UserData
+        {
+            UserName = User.DisplayName,
+            Email = User.Email
+        };
+
+        var firestore = FirebaseFirestore.DefaultInstance;
+        firestore.Document(collection + "/" + User.UserId).SetAsync(userData);
+    }
+
+    public void GetData()
+    {
+        var firestore = FirebaseFirestore.DefaultInstance;
+
+        firestore.Document(collection + "/" + User.UserId).GetSnapshotAsync().ContinueWithOnMainThread(task =>
+        {
+            Assert.IsNull(task.Exception);
+
+            var userData = task.Result.ConvertTo<UserData>();
+
+
+        });
+    }
+
+    public void LoginButton()
+    {
+        StartCoroutine(Login(emailLoginField.text, passwordLoginField.text));
+    }
+
+    public void RegisterButton()
+    {
+        StartCoroutine(Register(emailRegisterField.text, passwordRegisterField.text, usernameRegisterField.text));
+    }
+
+    private IEnumerator Login(string _email, string _password)
+    {
+        var LoginTask = auth.SignInWithEmailAndPasswordAsync(_email, _password);
+        yield return new WaitUntil(predicate: () => LoginTask.IsCompleted);
+
+        if (LoginTask.Exception != null)
+        {
+            Debug.LogWarning(message: $"Failed to register task with {LoginTask.Exception}");
+            FirebaseException firebaseEx = LoginTask.Exception.GetBaseException() as FirebaseException;
+            AuthError errorCode = (AuthError) firebaseEx.ErrorCode;
+
+            string message = "Login Failed!";
+            switch (errorCode)
+            {
+                case AuthError.MissingEmail:
+                    message = "Missing Email";
+                    break;
+                case AuthError.MissingPassword:
+                    message = "Missing Password";
+                    break;
+                case AuthError.WrongPassword:
+                    message = "Wrong Password";
+                    break;
+                case AuthError.InvalidEmail:
+                    message = "Invalid Email";
+                    break;
+                case AuthError.UserNotFound:
+                    message = "Account does not exist";
+                    break;
+            }
+            if(confirmLoginText.text != "")
+            {
+                confirmLoginText.text = "";
+            }
+            warningLoginText.text = message;
+        }
+        else
+        {
+            User = LoginTask.Result.User;
+            Debug.LogFormat("User signed in successfully: {0} ({1})", User.DisplayName, User.Email);
+            warningLoginText.text = "";
+            confirmLoginText.text = "Logged In";
+            UIManager.instance.LoggedInScreen();
+            userInfoText.text = "Hi, " + User.DisplayName + " User ID: " + User.UserId;
+        }
+    }
+
+    private IEnumerator Register(string _email, string _password, string _username)
+    {
+        if(_username == "")
+        {
+            warningRegisterText.text = "Missing Username";
+        }
+        else if (passwordRegisterField.text != passwordRegisterVerifyField.text)
+        {
+            warningRegisterText.text = "Password does not match";
+        }
+        else
+        {
+            var RegisterTask = auth.CreateUserWithEmailAndPasswordAsync(_email, _password);
+
+            yield return new WaitUntil(predicate: () => RegisterTask.IsCompleted);
+
+            if (RegisterTask.Exception != null)
+            {
+                Debug.LogWarning(message: $"Failed to register task with {RegisterTask.Exception}");
+                FirebaseException firebaseEx = RegisterTask.Exception.GetBaseException() as FirebaseException;
+                AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
+
+                string message = "Register Failed";
+                switch (errorCode)
+                {
+                    case AuthError.MissingEmail:
+                        message = "Missing Email";
+                        break;
+                    case AuthError.MissingPassword:
+                        message = "Missing Password";
+                        break;
+                    case AuthError.WeakPassword:
+                        message = "Weak Password";
+                        break;
+                    case AuthError.EmailAlreadyInUse:
+                        message = "Email alredy in use";
+                        break;
+                }
+                warningRegisterText.text = message;
+            }
+            else
+            {
+                User = RegisterTask.Result.User;
+
+                if (User != null)
+                {
+                    UserProfile profile = new UserProfile { DisplayName = _username };
+
+                    var ProfileTask = User.UpdateUserProfileAsync(profile);
+
+                    yield return new WaitUntil(predicate: () => ProfileTask.IsCompleted);
+
+                    if (ProfileTask.Exception != null) 
+                    { 
+                        Debug.LogWarning(message: $"Failed to register task with {ProfileTask.Exception}");
+                        FirebaseException firebaseEx = ProfileTask.Exception.GetBaseException() as FirebaseException;
+                        AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
+                        warningRegisterText.text = "Username set failed!";
+                    }
+                    else
+                    {
+                        SetData();
+                        UIManager.instance.Back();
+                        warningRegisterText.text = "";
+                    }
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -27,14 +27,26 @@ public class UIManager : MonoBehaviour
 
     public void Register()
     {
+        for (int i = 0; i < LoginPanel.transform.childCount; i++)
+        {
+            GameObject child = LoginPanel.transform.GetChild(i).gameObject;
+            if (child.GetComponent<TMP_InputField>() != null)
+                child.GetComponent<TMP_InputField>().text = "";
+        }
         LoginPanel.SetActive(false);
         RegisterPanel.SetActive(true);
     }
 
     public void Back()
     {
-        LoginPanel.SetActive(true);
+        for (int i = 0; i < RegisterPanel.transform.childCount; i++)
+        {
+            GameObject child = RegisterPanel.transform.GetChild(i).gameObject;
+            if (child.GetComponent<TMP_InputField>() != null)
+                child.GetComponent<TMP_InputField>().text = "";
+        }
         RegisterPanel.SetActive(false);
+        LoginPanel.SetActive(true);
     }
 
     public void LoggedInScreen()
